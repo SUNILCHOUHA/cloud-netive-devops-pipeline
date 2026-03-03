b6023906a4f14cfcc0cfe097812e6a33c80f5d13
Sunil Chouhan <chouhansunilm@gmail.com>
2026-03-03 11:57:33 +0530

    Add ArgoCD Image Updater

--- terraform/argocd-image-updater.tf
+++ terraform/argocd-image-updater.tf
@@ -1,44 +1,45 @@
-resource "helm_release" "argocd_image_updater" {
-  name       = "argocd-image-updater"
-  namespace  = "argocd"
+# resource "helm_release" "argocd_image_updater" {
+#   name       = "argocd-image-updater"
+#   namespace  = "argocd"
 
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argocd-image-updater"
-  version    = "0.10.0"
+#   repository = "https://argoproj.github.io/argo-helm"
+#   chart      = "argocd-image-updater"
+#   version    = "0.10.0"
 
-  force_update = true
-  recreate_pods = true
-  cleanup_on_fail = true
+#   force_update = true
+#   recreate_pods = true
+#   cleanup_on_fail = true
 
-  values = [
-    yamlencode({
-      serviceAccount = {
-        create = true
-        name   = "argocd-image-updater"
-        annotations = {
-          "eks.amazonaws.com/role-arn" = aws_iam_role.argocd_image_updater_role.arn
-        }
-      }
+#   values = [
+#     yamlencode({
+#       serviceAccount = {
+#         create = true
+#         name   = "argocd-image-updater"
+#         annotations = {
+#           "eks.amazonaws.com/role-arn" = aws_iam_role.argocd_image_updater_role.arn
+#         }
+#       }
 
-      config = {
-        logLevel = "info"
-      }
+#       config = {
+#         logLevel = "debug"
+#       }
 
-      extraArgs = [
-        "--interval=2m"
-      ]
+#       extraArgs = [
+#         "--interval=2m",
+#         "--aws-region=ap-south-1"
+#       ]
 
-      extraEnv = [
-        {
-          name  = "AWS_REGION"
-          value = "ap-south-1"
-        }
-      ]
-    })
-  ]
+#       extraEnv = [
+#         {
+#           name  = "AWS_REGION"
+#           value = "ap-south-1"
+#         }
+#       ]
+#     })
+#   ]
 
-  depends_on = [
-    helm_release.argocd,
-    aws_iam_role_policy_attachment.argocd_image_updater_ecr_attach
-  ]
-}
+#   depends_on = [
+#     helm_release.argocd,
+#     aws_iam_role_policy_attachment.argocd_image_updater_ecr_attach
+#   ]
+# }
